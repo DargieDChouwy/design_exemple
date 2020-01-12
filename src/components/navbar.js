6ac33928b718e961c348d824ee1c29307df904b7
Mawie <eigrads@hotmail.com>
2020-01-12 20:51:23 +0100

    navbar, dropdown, link button responsif/ time:1h

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,10 +4,37 @@ class navbar extends React.Component {
     state = {  }
     render() { 
         return ( 
-        <nav>
-            <a href="#">{this.props.name}</a>
-            <a href="#">{this.props.name}</a>
-            <a href="#">{this.props.name}</a>
+        <nav id="mainNav">
+            <ul>
+            <div id="navImg"></div>
+            <li><a href="#">{this.props.textOne}</a></li>
+            <li><a href="#">{this.props.textSecond}</a></li>
+            <div id="dropdown">
+                    <li>About the firm 
+                        <i id="fa fa-caret-down"></i>
+                    </li>
+                    <div id="dropdown-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                    </div>
+                </div>
+            <li><a href="#">{this.props.textFour}</a></li>
+            </ul>
+            <div id="navRight">
+                <div id="dropdown">
+                    <span>English 
+                        <i id="fa fa-caret-down"></i>
+                    </span>
+                    <div id="dropdown-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                    </div>
+                </div> 
+                <span>Login</span>
+                <button id="newButton">Register</button>
+            </div>
         </nav> );
     }
 }
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import '../styles/App.css';
 import About from '../components/about'
+import Navbar from '../components/navbar'
 
 function App() {
   return (
     <div>
-      <About
-      title="About Exemplar"
-      text="We're a real estate technology company whose goal is to empower investors with direct access to fully vetted, institunional-grade real estate investments faster and smarter than ever before."
-      textLeft="Our Investment Team brings over 20 billion CZK in real estate transaction experience from leading firms like Blackstone and Goldman Sachs - while our engineering and products teams hail form leading tech companies like Dropbox and Square."
-      textRight="With in-depth experience in real estate, combined with cutting-edge technology and close local operator partnerships, Exemplar brings you access to institutional-grade investments with greater transparency and lower fees. Exemplar brings you access to institutional-grade investments with greater transparency and lower fees. "
-      />
+      <header>
+        <Navbar 
+        textOne="Investments"
+        textSecond="How to invest"
+        textFour="Insights"
+        />
+      </header>
+        <About
+        title="About Exemplar"
+        text="We're a real estate technology company whose goal is to empower investors with direct access to fully vetted, institunional-grade real estate investments faster and smarter than ever before."
+        textLeft="Our Investment Team brings over 20 billion CZK in real estate transaction experience from leading firms like Blackstone and Goldman Sachs - while our engineering and products teams hail form leading tech companies like Dropbox and Square."
+        textRight="With in-depth experience in real estate, combined with cutting-edge technology and close local operator partnerships, Exemplar brings you access to institutional-grade investments with greater transparency and lower fees."
+        />
     </div>
   );
 }
